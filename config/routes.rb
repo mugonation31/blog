ea8dcb2417d3a969b0676e830f55d7ba4e7f753d
Simbarashe Mugomezi <108476650+mugonation31@users.noreply.github.com>
2024-05-22 20:24:14 +0100

    completed Rails blog app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
-  get "/cars", to: "cars#index"
+  root "articles#index"
+
+  resources :articles do 
+    resources :comments
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
